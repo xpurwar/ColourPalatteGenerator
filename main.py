453882398896878fc01a4db9c90955ce6f94bd93
Anna <purwar.ananya4@gmail.com>
2024-11-20 15:29:48 -0800

    feat: cann add cutom colours now

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import numpy as np
+import matplotlib as mpl
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 
-noise = PerlinNoise(octaves=10, seed=9)
+noise = PerlinNoise(octaves=3, seed=8.90)
 xpix, ypix = 150, 150
 pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
-
-plt.imshow(pic, cmap='Paired')
+my_cmap = ListedColormap(["darkorange", "gold", "lawngreen", "lightseagreen"])
+plt.imshow(pic, cmap = my_cmap)
 font1 = {'family':'serif','color':'black','size':18}
 plt.title("Goodreads aura :0", fontdict = font1)
 plt.show()
